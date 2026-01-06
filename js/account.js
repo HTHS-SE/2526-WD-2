e4fb2dd28be447032ed3ecc0c62d57c8ada437f5
timsaks <timotheysaks2008@gmail.com>
2026-01-06 13:48:27 -0500

    Added Welcome Message

---
+++ js/account.js
@@ -0,0 +1,57 @@
+// This JS file is for registering a new app user ---------------------------//
+
+// ----------------- Firebase Setup & Initialization ------------------------//
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+    from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+  import {getDatabase, ref, set, update, child, get}
+    from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD2xfIz6jMWHXnRpMW5afLkNYPZllbBDdc",
+    authDomain: "wd-2-fc227.firebaseapp.com",
+    databaseURL: "https://wd-2-fc227-default-rtdb.firebaseio.com",
+    projectId: "wd-2-fc227",
+    storageBucket: "wd-2-fc227.firebasestorage.app",
+    messagingSenderId: "31083722430",
+    appId: "1:31083722430:web:87e89224c22a69793450ef"
+    };
+
+  // Initialize Firebase
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Firebase Authentication
+  const auth = getAuth();
+
+  // Return instance of your app's firebase realtime database (FRD)
+  const db = getDatabase(app);
+
+function getUsername() {
+  // Grab value for the 'keep logged in switch'
+  let keepLoggedIn = localStorage.getItem("keepLoggedIn");
+
+  // Grab user information passed from signIn.js
+  if (keepLoggedIn == 'yes') {
+    currentUser = JSON.parse(localStorage.getItem('user'));
+  } else {
+    currentUser = JSON.parse(sessionStorage.getItem('user'));
+  }
+}
+
+let currentUser = null;
+
+window.onload = function() {
+    getUsername()
+    document.getElementById("accountHeading").innerText = "Welcome, " + currentUser.firstName;
+}
