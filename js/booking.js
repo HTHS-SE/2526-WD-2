849c1de193a0422c682fabc161ddc93e7e3da953
Jody Gan <jodylaura.gan@gmail.com>
2026-01-13 12:29:56 -0500

    Booking JS (not done)
    - updated the ids in the booking HTML
    - not in a testable state yet

--- js/booking.js
+++ js/booking.js
@@ -1,3 +1,5 @@
+// Booking Page JS
+
 // ----------------- Firebase Setup & Initialization ------------------------//
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
@@ -37,115 +39,62 @@
   // Return instance of your app's firebase realtime database (FRD)
   const db = getDatabase(app);
 
-function getUsername() {
-  // Grab value for the 'keep logged in switch'
-  let keepLoggedIn = localStorage.getItem("keepLoggedIn");
-
-  // Grab user information passed from signIn.js
-  if (keepLoggedIn == 'yes') {
-    currentUser = JSON.parse(localStorage.getItem('user'));
-  } else {
-    currentUser = JSON.parse(sessionStorage.getItem('user'));
-  }
-}
-
-// Sign-out function that will remove user info from local/session storage and
-// sign-out from FRD
-function SignOutUser(){
-  sessionStorage.removeItem('user');        // Clear session storage
-  localStorage.removeItem('user');          // Clear local storage
-  localStorage.removeItem('keepLoggedIn');
-
-  signOutLink(auth).then(() => {
-    // Sign out successful
-  }).catch((error) => {
-    // Error occured
-  });
-
-  window.location = "home.html"
-}
-
 // ------------------------Set (insert) data into FRD ------------------------
-function setData(userID, year, month, day, temperature){
+function setData(userID, event, museum, year, month, day, party){
   // Must use brackets around variable name to use it as a key
-  set(ref(db, 'users/' + userID + '/data/' + year + '/' + month),{
-    [day]: temperature
+  set(ref(db, 'users/' + userID + '/data/' + event + '/' + museum + '/' + year + '/' + month),{
+    [day]: party
   })
   .then(() => {
     alert("Data stored successfully!");
   })
   .catch((error) => {
-    alert("there was an error. Error: " + error)
+    alert("There was an error. Error: " + error)
   });
 }
 
 // -------------------------Update data in database --------------------------
-function updateData(userID, year, month, day, temperature){
+function updateData(userID, event, museum, year, month, day, party){
   // Must use brackets around variable name to use it as a key
   // Appends data to the month instead of wiping the month's information (setData)
-  update(ref(db, 'users/' + userID + '/data/' + year + '/' + month),{
-    [day]: temperature
+  update(ref(db, 'users/' + userID + '/data/' + event + '/' + museum + '/' + year + '/' + month),{
+    [day]: party
   })
   .then(() => {
     alert("Data stored successfully!");
   })
   .catch((error) => {
-    alert("there was an error. Error: " + error)
+    alert("There was an error. Error: " + error)
   });
 }
 
 // --------------------------- Home Page Loading -----------------------------
 window.onload = function(){
-  // ------------------------- Set Welcome Message -------------------------
-  getUsername();    // Get current user's first name
-  if(currentUser == null){
-    userLink.innerText = "Create New Account";
-    userLink.classList.replace("nav-link", "btn");
-    userLink.classList.add("btn-primary");
-    userLink.href = "register.html";
-
-    signOutLink.innerText = "Sign In";
-    signOutLink.classList.replace("nav-link", "btn");
-    signOutLink.classList.add("btn-success");
-    signOutLink.href = "signIn.html";
-  }
-  else {
-    userLink.innerText = currentUser.firstname;
-    welcome.innerText = "Welcome" + currentUser.firstname;
-    userLink.classList.replace("btn", "nav-link");
-    userLink.classList.add("btn-primary");
-    userLink.href = "#";
-
-    signOutLink.innerText = "Sign Out";
-    signOutLink.classList.replace("btn", "nav-link");
-    signOutLink.classList.add("btn-success");
-    this.document.getElementById('signOut').onclick = function(){
-      SignOutUser();
-    }
-  }
-
   // Set and Update Reservation Data in FRD
   // Set (Insert) data function call
   document.getElementById('set').onclick = function(){
+    const event = document.getElementById('event').value;
+    const museum = document.getElementById('museum').value;
     const year = document.getElementById('year').value;
     const month = document.getElementById('month').value;
     const day = document.getElementById('day').value;
-    const temperature = document.getElementById('temperature').value;
+    const party = document.getElementById('party').value;
     const userID = currentUser.uid;
 
-    setData(userID, year, month, day, temperature);
+    setData(userID, event, museum, year, month, day, party);
   };
-
 
   // Update data function call
   document.getElementById('update').onclick = function(){
+    const event = document.getElementById('event').value;
+    const museum = document.getElementById('museum').value;
     const year = document.getElementById('year').value;
     const month = document.getElementById('month').value;
     const day = document.getElementById('day').value;
-    const temperature = document.getElementById('temperature').value;
+    const party = document.getElementById('party').value;
     const userID = currentUser.uid;
 
-    updateData(userID, year, month, day, temperature)
+    updateData(userID, event, museum, year, month, day, party)
   }
 
 //   // Get a datum function call
