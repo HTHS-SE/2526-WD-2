6f43bcf57c6ad681dd45bc1867e3b63985b2d2ad
timsaks <timotheysaks2008@gmail.com>
2026-01-13 13:50:27 -0500

    getMonthCounts function

--- js/account.js
+++ js/account.js
@@ -91,6 +91,139 @@ function getData(userID, year, month, day) {
   });
 }
 
+function monthNumberToName(monthNumber) {
+  return new Date(2000, monthNumber - 1).toLocaleString("en-US", {
+    month: "long"
+  });
+}
+
+async function getMonthCounts(year, uid) {
+  console.log("getMonthCounts ran");
+  const yearRef = ref(db, `users/${uid}/data/${year}`);
+  const snapshot = await get(yearRef);
+
+  const monthCounts = {};
+
+  if (snapshot.exists()) {
+    
+    snapshot.forEach((monthSnap) => {
+      const month = monthNumberToName(parseInt(monthSnap.key));
+
+      const value = monthSnap.val();
+      const dayCount = value ? Object.keys(value).length : 0;
+
+      monthCounts[month] = dayCount;
+      console.log(month, dayCount);
+    });
+  }
+  return monthCounts;
+}
+
+
+async function createChart(year, uid) {
+    const data = await getMonthCounts(year, uid); // createChart will wait for getData() to process the CSV file
+    const lineChart = document.getElementById('lineChart');
+    const myChart = new Chart(lineChart, {  // Construct the chart    
+        type: 'line',
+        data: {                         // Define data
+            labels: data.xYears,        // x-axis labels
+            datasets: [                 // Each object describes one dataset of y-values
+                                        //  including display properties.  To add more datasets, 
+                                        //  place a comma after the closing curly brace of the last
+                                        //  data set object and add another dataset object. 
+                {
+                    label:    `Combined Global LSA and SSW Temperature in ${degreeSymbol}C`,     // Dataset label for legend
+                    data:     data.yTemps,    
+                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                    backgroundColor:  'rgba(255, 0, 132, 0.2)',    // Color for data marker
+                    borderColor:      'rgba(255, 0, 132, 1)',      // Color for data marker border
+                    borderWidth:      1   // Data marker border width
+                },
+                {
+                    label:    `Combined NH LSA and SSW Temperature in ${degreeSymbol}C`,     // Dataset label for legend
+                    data:     data.yNHtemps,    
+                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                    backgroundColor:  'rgba(0, 102, 255, 0.2)',    // Color for data marker
+                    borderColor:      'rgba(0, 102, 255, 1)',      // Color for data marker border
+                    borderWidth:      1   // Data marker border width
+                },
+                {
+                    label:    `Combined SH LSA and SSW Temperature in ${degreeSymbol}C`,     // Dataset label for legend
+                    data:     data.ySHtemps,    
+                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                    backgroundColor:  'rgba(0, 153, 51, 0.2)',    // Color for data marker
+                    borderColor:      'rgba(0, 153, 51, 1)',      // Color for data marker border
+                    borderWidth:      1   // Data marker border width
+                },
+        ]
+        },
+        options: {                        // Define display chart display options 
+            responsive: true,             // Re-size based on screen size
+            maintainAspectRatio: false,
+            scales: {                     // Display options for x & y axes
+                x: {                      // x-axis properties
+                    title: {
+                        display: true,
+                        text: 'Year',     // x-axis title
+                        font: {                   // font properties
+                            size: 14
+                        },
+                    },
+                    ticks: {                      // x-axis tick mark properties
+                        callback: function(value, index) {
+                            // Set tick marks at every 5 years
+                            return index % 5 === 0 ? this.getLabelForValue(value) : '';
+                        },
+                        font: {
+                            size: 14
+                        },
+                    },
+                    grid: {                       // x-axis grid properties
+                        color: '#6c767e'
+                    }
+                },
+                y: {                              // y-axis properties
+                    title: {
+                        display: true,
+                        text: 'Global Mean Temperatures (°C)',     // y-axis title
+                        font: {
+                            size: 14
+                        },
+                    },
+                    ticks: {
+                        min: 0,
+                        maxTicksLimit: data.yTemps.length / 10,        // Actual value can be set dynamically
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {                       // y-axis gridlines
+                        color: '#6c767e'
+                    }
+                }
+            },
+            plugins: {                  // Display options for title and legend
+                title: {
+                    display: true,
+                    text: 'Global Mean Temperature vs. Year (since 1880)',
+                    font: {
+                        size: 24,
+                    },
+                    color: '#black',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom',
+                }
+            }
+        }       
+    });
+
+}
 
 
 
@@ -101,14 +234,13 @@ let currentUser = null;
 
 // --------------------------- Home Page Loading -----------------------------
 window.onload = function() {
-
+  console.log("Account page onload function ran");
+  getMonthCounts();
 
   
   getUsername();  // Get current user's first name
   
   // Update navbar
-  
-
   userLinkText.innerText = "Sign Out";
   userLink.href = "index.html";
   this.document.getElementById('userLink').onclick = function() {
@@ -129,7 +261,9 @@ window.onload = function() {
     getData(userID, year, month, day);
   */
 
-  
-    }
+  // Get user id for getMonthCounts
+  const userID = currentUser.uid;
+  console.log(getMonthCounts("2026", userID));
 
 
+}
